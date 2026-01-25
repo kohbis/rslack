86b4a9e9898f574b393a32bd5425f508118ebae8
kohbis <18735471+kohbis@users.noreply.github.com>
2026-01-25 16:01:27 +0900

    feat: 🎸 Add read mode for Slack channels to fetch and display message history

--- src/console/mod.rs
+++ src/console/mod.rs
@@ -1,7 +1,9 @@
 mod channel_selector;
 mod editor;
+mod message_viewer;
 mod table;
 
 pub use channel_selector::{ChannelSelector, SelectionResult};
 pub use editor::{Editor, EditorResult};
+pub use message_viewer::{print_messages, MessageViewer};
 pub use table::Table;
